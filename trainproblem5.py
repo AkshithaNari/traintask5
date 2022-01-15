1f673c00bfeedc09d1c79ce66548de12010b6b4e
AkshithaNari <akshithakylie@gmail.com>
2022-01-15 22:34:27 +0530

    first commit

---
+++ trainproblem5.py
@@ -0,0 +1,4 @@
+train_speed=int(input("train speed in kmph:"))
+time_to_cross_pole=int(input("time taken to cross pole in sec:"))
+dist=(train_speed*time_to_cross_pole)*5/18
+print("length of the train in m:",dist)
